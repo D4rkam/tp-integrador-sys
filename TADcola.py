ab5f90ff1f0321fd7a6c83b7ca5861968c2fa43e
EABarrios <barrios.ezequiel.adrian@gmail.com>
2026-04-30 13:39:43 -0300

    Especificacion del TAD

---
+++ TADcola.py
@@ -0,0 +1,22 @@
+# === crear cola ===
+def crearCola():
+    
+# === Encolar ===
+def encolar(cola, elem):
+    
+    
+# === desencolar ===
+def desencolar(cola):
+
+
+# === cola Vacia ===
+def colaVacia(cola):
+    
+
+# === contar elementos ===
+def tamanioCola(cola):
+
+
+# === copiar cola ===
+def copiarCola(cola1, cola2):
+    
