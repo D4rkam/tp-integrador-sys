40d976e99f0435f99d4053933d0bc6e8bdef6e30
santitalone31 <santiagotalone7@gmail.com>
2026-04-30 13:15:14 -0300

    Especificacion del TAD Area de investigacion

---
+++ TADareaInvestigacion.py
@@ -0,0 +1,38 @@
+#"--- TAD AREA DE INVESTIGACIÓN ---
+
+def crearAreaDeInvestigacion():
+    # Crea un área de investigación
+    pass
+
+
+def AgregarInvestigador(areainvestigacion, investigador):
+    #Agrega al investigador al Área de investigacion
+    pass
+
+
+def EliminarInvestigador(areainvestigacion, investigador):
+    #Elimina al investigador del Área de Investigación
+    pass
+
+def existeInvestigador(areainvestigacion, investigador):
+    #Retorna True o False si el investigador pertenece al Área de Investigacion
+ pass
+
+def tamanioAreaInvestigacion(areainvestigacion):
+    #Retorna la cantidad de investigadores que hay en el Área de investigacion
+    pass
+
+def recuperarInvestigador(areainvestigacion, i):
+    #Retorna el alumno de la posición iésima
+    pass
+
+
+
+ 
+
+
+
+
+
+
+
