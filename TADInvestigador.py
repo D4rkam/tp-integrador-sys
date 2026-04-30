c6815f86c88ebcedb0270bff6e43f2058fa75a83
Thomas Linares <thomaslinares32@gmail.com>
2026-04-30 12:43:14 -0300

    Especificacion del TAD Investigador

---
+++ TADInvestigador.py
@@ -0,0 +1,99 @@
+# ==== Crear ====
+def crearInvestigador():
+    """Crea un investigador vacio"""
+    pass
+
+
+# ==== Carga de datos ====
+def cargarNombre(investigador, nombre):
+    """Recibe un investigador le carga el nombre"""
+    pass
+
+
+def cargarApellido(investigador, apellido):
+    """Recibe un investigador le carga el apellido"""
+    pass
+
+
+def cargarNroLegajo(investigador, num_legajo):
+    """Recibe un investigador le carga el legajo"""
+
+    pass
+
+
+def cargarFechaIngreso(investigador, fecha_ingreso):
+    """Recibe un investigador le carga la fecha de ingreso"""
+
+    pass
+
+
+def cargarNroLab(investigador, num_laboratorio):
+    """Recibe un investigador le carga el numero de lab"""
+
+    pass
+
+
+# ==== Ver datos ====
+def verNombre(investigador):
+    """Recibe un investigador y retorna su nombre"""
+    pass
+
+
+def verApellido(investigador):
+    """Recibe un investigador y retorna su apellido"""
+    pass
+
+
+def verNroLegajo(investigador):
+    """Recibe un investigador y retorna su numero de legajo"""
+
+    pass
+
+
+def verFechaIngreso(investigador):
+    """Recibe un investigador y retorna su fecha de ingreso"""
+
+    pass
+
+
+def verNroLab(investigador):
+    """Recibe un investigador y retorna su numero de laboratorio"""
+
+    pass
+
+
+# ==== Modificar datos ====
+def modificarNombre(investigador, nombre):
+    """Recibe un investigador y modifica el nombre al nuevo recibido"""
+
+    pass
+
+
+def modificarApellido(investigador, apellido):
+    """Recibe un investigador y modifica el apellido al nuevo recibido"""
+
+    pass
+
+
+def modificarNroLegajo(investigador, num_legajo):
+    """Recibe un investigador y modifica el numero de legajo al nuevo recibido"""
+
+    pass
+
+
+def modificarFechaIngreso(investigador, fecha_ingreso):
+    """Recibe un investigador y modifica la fecha de ingreso al nuevo recibido"""
+
+    pass
+
+
+def modificarNroLab(investigador, num_lab):
+    """Recibe un investigador y modifica el numero de laboratorio al nuevo recibido"""
+
+    pass
+
+
+def asignarInvestigador(investigador1, investigador2):
+    """Copia los datos del investigar 1 al investigador 2"""
+
+    pass
